e7ee6400a7ade22031105a82b51555ccd618e471
Ferdinand Lazarte <ferdinand.lazarte@tell.com>
2020-04-06 23:22:25 +0800

    Adding maps

--- wp-content/themes/virtualninjaswork/template-parts/contact.php
+++ wp-content/themes/virtualninjaswork/template-parts/contact.php
@@ -61,7 +61,7 @@ $virtual_email = $company->fax;
           </div>
 
           <div class="col-md-6 d-flex">
-          	<div id="map" class="bg-white"></div>
+          	<div id="map" class="bg-white"><iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3865.0503690261653!2d121.01761091431791!3d14.366497236623472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sCJA%20Building%20B5-G%20L2%20Bach%20St.%20Jade%20Ave.%20Jade%20Heights%20Tunasan%2CMuntinlupa%2C1773!5e0!3m2!1sen!2sph!4v1586185999159!5m2!1sen!2sph" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
           </div>
         </div>
       </div>
