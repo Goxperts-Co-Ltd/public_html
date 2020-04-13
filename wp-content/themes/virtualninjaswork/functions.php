8dd0c118227a83b7b13908150c8e84fae028ba09
Ferdinand Lazarte <ferdinand.lazarte@tell.com>
2020-04-13 23:53:14 +0800

    Login page and restrictions

--- wp-content/themes/virtualninjaswork/functions.php
+++ wp-content/themes/virtualninjaswork/functions.php
@@ -569,3 +569,64 @@ function mo_remove_default_comment_field( $defaults ) { if ( isset( $defaults[ '
 
 // // Remove default link around product entries
 // remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+add_action('template_redirect', 'my_check_login');
+function my_check_login(){
+    if( is_page('login') && isset($_POST['login_Sbumit']) ){
+    // check input and
+    // do my login stuff here 
+    $creds                  = array();
+	$creds['user_login']    = stripslashes( trim( $_POST['userName'] ) );
+	$creds['user_password'] = stripslashes( trim( $_POST['passWord'] ) );
+	$creds['remember']      = isset( $_POST['rememberMe'] ) ? sanitize_text_field( $_POST['rememberMe'] ) : '';
+	$redirect_to            = esc_url_raw( $_POST['redirect_to'] );
+	$secure_cookie          = null;
+		
+	if($redirect_to == '')
+		$redirect_to= get_site_url(). '/dashboard/' ; 
+		
+		if ( ! force_ssl_admin() ) {
+			$user = is_email( $creds['user_login'] ) ? get_user_by( 'email', $creds['user_login'] ) : get_user_by( 'login', sanitize_user( $creds['user_login'] ) );
+
+		if ( $user && get_user_option( 'use_ssl', $user->ID ) ) {
+			$secure_cookie = true;
+			force_ssl_admin( true );
+		}
+	}
+
+	if ( force_ssl_admin() ) {
+		$secure_cookie = true;
+	}
+
+	if ( is_null( $secure_cookie ) && force_ssl_login() ) {
+		$secure_cookie = false;
+	}
+
+	$user = wp_signon( $creds, $secure_cookie );
+
+	if ( $secure_cookie && strstr( $redirect_to, 'wp-admin' ) ) {
+		$redirect_to = str_replace( 'http:', 'https:', $redirect_to );
+	}
+
+	if ( ! is_wp_error( $user ) ) {
+		wp_safe_redirect( $redirect_to );			
+	} else {			
+		if ( $user->errors ) {
+			$errors['invalid_user'] = __('<strong>ERROR</strong>: Invalid user or password.'); 
+		} else {
+			$errors['invalid_user_credentials'] = __( 'Please enter your username and password to login.', 'kvcodes' );
+		}
+	}		   
+	}
+	return $errors;
+}
+
+//restrict login user to login page
+function redirect_login_page() {
+
+    if( is_page( 'login' ) && is_user_logged_in() ) {
+        wp_redirect( home_url() );
+        exit;
+    }
+
+}
+add_action( 'template_redirect', 'redirect_login_page' );
---
+++ wp-content/themes/virtualninjaswork/page-login.php
@@ -0,0 +1,67 @@
+<?php  
+/** 
+ * Template Name: Login Page
+ */  
+ get_header(); 
+ global $post;
+$post_array = $post->to_array();
+//get the banner image
+$image = get_field( 'banner_image',$post_array['ID']);
+?>
+<div class="hero-wrap hero-wrap-2" style="background-image: url('<?php echo $image['url']; ?>');"
+    data-stellar-background-ratio="0.5">
+    <div id="job-opens" class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-end justify-content-start">
+            <div class="col-md-12 ftco-animate text-center mb-5">
+                <h2 class="mb-3 bread">Login</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="ftco-section contact-section bg-light">
+      <div class="container">
+        <div class="row d-flex mb-5 contact-info">
+          <div class="col-md-12 mb-4">
+            <h3 class="h3">Login Information</h3>
+          </div>
+          <div class="w-100"></div>
+        </div>
+        <div class="row block-9">
+          <div class="col-md-6 order-md-last d-flex">
+            <form name="loginform" action="<?php echo home_url(); ?>/login" method="post" class="bg-white p-5 contact-form">
+            <div>
+            <?php 
+             $errors = my_check_login();
+            if(!empty($errors)) {  //  to print errors,
+                foreach($errors as $err )
+                    echo '<p style="color:red;">'.$err.'</p>';
+            } ?>
+            </div>
+              <div class="form-group">
+                <input type="text"  name="userName" id="user_login" class="form-control" placeholder="Username" value="">
+              </div>
+              <div class="form-group">
+                <input type="password" name="passWord" id="user_pass" class="form-control" placeholder="Password" value="">
+              </div>
+              <div>
+              <p class="login-remember"><label><input name="rememberMe" type="checkbox" id="rememberme" value="forever"> Remember Me</label>
+              </div>
+              <div class="form-group">
+               <input type="hidden" name="login_Sbumit" >
+                <input type="submit" name="wp-submit" id="wp-submit" class="btn btn-primary py-3 px-5" value="Log In">
+                <input type="hidden" name="redirect_to" value="<?php echo home_url(); ?>/wp-admin/index.php">
+              </div>
+            </form>         
+          </div>
+
+          <div class="col-md-6 d-flex">
+          	<div class="bg-white"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+ <?php
+  get_footer(); 
