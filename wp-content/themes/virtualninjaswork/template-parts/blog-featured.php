8409365026cd63da4b924e5737bca1b52a1eca28
Ferdinand Lazarte <ferdinand.lazarte@tell.com>
2020-04-30 23:39:54 +0800

    BLog fix layout and set it dynamic

--- wp-content/themes/virtualninjaswork/template-parts/blog-featured.php
+++ wp-content/themes/virtualninjaswork/template-parts/blog-featured.php
@@ -78,80 +78,10 @@ $size = 'featured-post-img';
               </div>
             </div>
           </div>
-          <div class="col-lg-4">
-            <div class="section-title">
-              <h2>Trending</h2>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">01</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">02</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">03</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-
-            <div class="trend-entry d-flex">
-              <div class="number align-self-start">04</div>
-              <div class="trend-contents">
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <div class="post-meta">
-                  <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
-                  <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-                </div>
-              </div>
-            </div>
-            
-            <p>
-              <a href="#" class="more">See All Trends <span class="icon-keyboard_arrow_right"></span></a>
-            </p>
-
-          </div>
+         <!-- re blogs -->
+           <?php get_template_part( 'template-parts/pillar-trending'); ?>
+        <!-- end of featured blogs -->
         </div>
       </div>
     </div>
     <!-- END section -->
-
-    <div class="py-0">
-      <div class="container">
-        <div class="half-post-entry d-block d-lg-flex bg-light">
-          <div class="img-bg" style="background-image: url('images/big_img_1.jpg')"></div>
-          <div class="contents">
-            <span class="caption">Editor's Pick</span>
-            <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-            <p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate vero obcaecati natus adipisci necessitatibus eius, enim vel sit ad reiciendis. Enim praesentium magni delectus cum, tempore deserunt aliquid quaerat culpa nemo veritatis, iste adipisci excepturi consectetur doloribus aliquam accusantium beatae?</p>
-            
-            <div class="post-meta">
-              <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">Food</a></span>
-              <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
