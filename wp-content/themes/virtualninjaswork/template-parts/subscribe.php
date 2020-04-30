38d7324ed11a8efd1c48af6b686a0a8326926455
Ferdinand Lazarte <ferdinand.lazarte@tell.com>
2020-04-30 09:20:08 +0800

    Subscription

--- wp-content/themes/virtualninjaswork/template-parts/subscribe.php
+++ wp-content/themes/virtualninjaswork/template-parts/subscribe.php
@@ -4,31 +4,27 @@
           <div class="row d-flex justify-content-center">
             <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
               <h2>Subscribe to our Newsletter</h2>
-              <p>Subscribe to emails to learn and be updated of the latest learning and opportunities!</p>
+              <p>Subscribe to emails to learn and be updated of the latest learning and opportunities!</p>   
+              <?php 
+                if (isset($_POST['subscribeSubmit'])) {
+                  $email = $_POST['subscribeEmail']; 
+                  $args = ['form_id' => 109 ];
+                  $fields = [
+                    'email_address_9377650294' => $email
+                  ];   
+                  $subscribed =  weforms_insert_entry($args, $fields );
+                  var_dump($subscribed);
+                }
+                  ?>   
               <div class="row d-flex justify-content-center mt-4 mb-4">
                 <div class="col-md-12">
+                <form action="" method="POST" class="subscribe-form">
                   <div class="subscribe-form">
                     <div class="form-group d-flex">
-                     <?php 
-
-                      $args = [
-                        'contact' => array(
-                          'email' => 'ferdinandlazarte20179@gmail.com',
-                        ),
-                        'groups' => '',
-                        'life_stage'=> '',
-                      ];
-
-                      $subscribe = new \WeDevs\ERP\CRM\Subscription;
-                     // \Subscription::get_instance();
-                    // var_dump( create_subsciber( $args));die;
-                     //echo do_shortcode('[weforms id="109"]')
-                     
-                     var_dump($subscribe->create_subsciber( $args));die;
-                     ?>
-                     <input type="text" class="form-control" placeholder="Enter email address">     
-                      <input type="submit" value="Submit" class="submit px-3" name="submit">
+                     <input type="text" class="form-control" placeholder="Enter email address" id="subscribeEmail" name="subscribeEmail">     
+                      <input type="submit" value="Submit" class="submit px-3" name="subscribeSubmit">
                     </div>
+                    </form>
                   </div>
                 </div>
               </div>
